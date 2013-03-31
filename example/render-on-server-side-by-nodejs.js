b4868c0afcbf06db451a83affcbf4d2c2918e0bf
alee chou <zq@alee2002.com>
2013-03-31 23:22:02 +0800

    重新实现了模板渲染方式，处于效率考虑，使用传统的过程式渲染方式

--- example/render-on-server-side-by-nodejs.js
+++ example/render-on-server-side-by-nodejs.js
@@ -1,13 +1,11 @@
 
 
 var TemplateCaches = require("ocTemplate") ;
+var RenderBuffer = require("ocTemplate/lib/RenderBuffer.js") ;
 var ui = require("ocTemplate").singleton ;
 
 
-
-var tpl = ui.template("ocTemplate/example/templates/template-a.html",__filename) ;
-
-tpl.load(function(err,tpl){
+var tpl = ui.template("ocTemplate/example/templates/template-a.html",function(err,tpl){
 	if(err)
 	{
 		console.log(err.stack) ;
@@ -22,38 +20,73 @@ tpl.load(function(err,tpl){
 		, x: 1000
 		, i: 1
 		, l: 0
+		, arr: [1,2,3]
+		, obj: {a:1,b:2,c:3}
 	} ;
 
-	tpl.createRenderContext().render(function(err,buff){
+	tpl.render(model,function(err,buff){
 		if(err)
 		{
+			console.log(err.message) ;
 			console.log(err.stack) ;
-			console.log(err.toString()) ;
 		}
 		console.log(buff.toString()) ;
-	},model) ;
-}) ;
+	}) ;
 
-//
-//tpl.display({
-//		a: "a"
-//		, b: 'b'
-//		, c: 'c'
-//		, d: 'd'
-//		, x: 1000
-//		, i: 0
-//		, l: 1
-//	},function(err,buff){
-//
-//		if(err)
-//		{
-//			console.log(err.stack) ;
-//			console.log(err.toString()) ;
-//		}
-//
+//	test(model,new RenderBuffer,function(err,buff){
 //		console.log(buff.toString()) ;
 //	}) ;
 
+}) ;
 
+
+
+function test($model,buff,callback){
+
+	var $variables = {};
+	$variables.__proto__ = $model;
+	function _$step_0(err)
+	{
+		if(err){ callback && callback(err) ; return ;}
+		var $nextstep = _$step_last ;
+		with($variables)
+		{try{
+			buff.write( "<div>a</div>\n\n<div>\n    <include file=\"ocTemplate/example/templates/template-b.html\" />\n</div>\n\n        <if \"\">\n            hi\n        </if>\n\n<div title=\"\" " );
+			buff.write( i )
+
+			buff.write( ">ccc</div>\n    " );
+			buff.write( i )
+
+			buff.write( "\n<div>dddd</div>\n\n<loop 10 var=i>\n    " );
+			buff.write( i )
+
+			buff.write( "\n</loop>\n\n\n<foreach \"\" var=\"v\" key=\"i\">\n    <foreach \"\" var=\"vv\" key=\"k\">\n        " );
+			buff.write( i )
+
+			buff.write( " : " );
+			buff.write( v )
+
+			buff.write( "\n        " );
+			buff.write( k )
+
+			buff.write( " : " );
+			buff.write( vv )
+
+			buff.write( "\n    </foreach>\n</foreach>" );
+
+
+			$nextstep(null) ;
+		}catch(err){
+			callback && callback(err) ;
+			return ;
+		}}
+	}
+
+	function _$step_last(err)
+	{
+		callback && callback(err||$lasterr,buff) ;
+	}
+	_$step_0(null) ;
+}
 
 
